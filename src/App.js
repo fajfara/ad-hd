6825bc410a03ba8eaa5d67e3187e15aa2f11365d
Živa Škof <zivadiva@Zias-MacBook-Air.local>
2019-05-18 15:01:01 +0200

    landing page and stzling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './scss/main.scss';
 
 // Components import
 import Home from "./pages/Home";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Ribbon from "../img/ribbon.png";
+
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        <div className="main-title">
+          <img src={ Ribbon } alt="Ribbon" className="main-title__ribbon"/>
+          <div className="main-title__left">
+            <h2>AD</h2>
+          </div>
+          <div className="main-title__center">
+            <i class="fas fa-bolt"></i>
+          </div>
+          <div className="main-title__right">
+            <h2>HD</h2>
+          </div>
+        </div>
+      </>
     )
   }
 }
