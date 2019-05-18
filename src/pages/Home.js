81a7b9d25a5a1ecb27696bd7f5fb1f068bb1d41a
Živa Škof <zivadiva@Zias-MacBook-Air.local>
2019-05-18 16:02:16 +0200

    updates scss styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ribbon from "../img/ribbon.png";
+import Monkey from "../img/monkey.png";
 
 
 export default class Home extends Component {
@@ -8,14 +9,22 @@ export default class Home extends Component {
       <>
         <div className="main-title">
           <img src={ Ribbon } alt="Ribbon" className="main-title__ribbon"/>
-          <div className="main-title__left">
-            <h2>AD</h2>
+          <img src={ Monkey } alt="Monkey" className="main-title__monkey"/>
+          <div className="main-title__top">
+            <div className="main-title__left">
+              <h2>AD</h2>
+            </div>
+            <div className="main-title__center">
+              <i class="fas fa-bolt"></i>
+            </div>
+            <div className="main-title__right">
+              <h2>HD</h2>
+            </div>
           </div>
-          <div className="main-title__center">
-            <i class="fas fa-bolt"></i>
-          </div>
-          <div className="main-title__right">
-            <h2>HD</h2>
+          <div className="main-title__bottom_text">
+            <p>
+              For those about <span>oh look a monkey</span>
+            </p>
           </div>
         </div>
       </>
